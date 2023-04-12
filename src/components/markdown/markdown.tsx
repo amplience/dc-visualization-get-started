3c95ce6d898b0c8880ac649cd567b0489c02d812
Daniel Nizynski <daniel.nizynski@pagepro.co>
2023-04-12 16:49:10 +0200

    fix: every content type is now rendering correctly

--- src/components/markdown/markdown.tsx
+++ src/components/markdown/markdown.tsx
@@ -1,26 +1,21 @@
-import {FC, HTMLAttributes, ReactNode} from "react";
+import { FC, HTMLAttributes, ReactNode } from 'react';
 
-import MarkdownJsx from "markdown-to-jsx";
-import { useRouter } from "next/router";
+import MarkdownJsx from 'markdown-to-jsx';
+import { useRouter } from 'next/router';
 
-import { MarkdownWrapper } from "./styles";
+import { MarkdownWrapper } from './styles';
 
 export type MarkdownProps = {
   Wrapper: FC<HTMLAttributes<HTMLDivElement>>;
-  stringWithMinMax?: string;
-  justString?: string;
+  text?: string;
 };
 
 export const Markdown: FC<MarkdownProps> = (props) => {
-  const {
-    Wrapper = MarkdownWrapper,
-    justString,
-    stringWithMinMax = "!! content missing !!"
-  } = props;
+  const { Wrapper = MarkdownWrapper, text } = props;
 
   return (
     <Wrapper>
-      <MarkdownJsx>{justString || stringWithMinMax}</MarkdownJsx>
+      <MarkdownJsx>{text}</MarkdownJsx>
     </Wrapper>
   );
 };
